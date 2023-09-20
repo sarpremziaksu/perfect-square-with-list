19bc8459dc2ef62a82c9c76246878eced53ef21c
Sarp Remzi AKSU <94607857+sarpremziaksu@users.noreply.github.com>
2023-09-20 20:49:56 +0300

    Update perfect_square.py
    Mazinde 100 sene yatar YAŞA KOPENHAG

--- perfect_square.py
+++ perfect_square.py
@@ -1,19 +1,34 @@
-import math
-
-# Listeyi oluşturma
-list =[]
-num = float(input("Kac tane rakam uzerinden islem yapmak istiyorsunuz?: "))
-for n in range(num)
-liste = float(input("Listede yer almasini istediginiz sayilari giriniz: "))
-list.append(liste)
-
-# Girilen sayıları yazdırma
-print("Girilen Sayilar: ", list)
-
-
-# Listeye ilave edilen sayıların tam kare olmadığını tespit eden metod
-tamKare = [k for k in list if (
-   math.sqrt(k) == math.floor(math.sqrt(k)))]
-
-# Listedeki bütün tam kare sayıları yazdırma
-print("Listedeki tam kare sayilar: ", tamKare)
+from math import ceil, sqrt
+ 
+# Belilenen aralıkta bütün tam kare sayıları yazdıran fonksiyon tanımı
+def tamKare(ilk, son) :
+ 
+ 
+    # İlk sayıyı elde etme
+    n1 = ceil(sqrt(ilk));
+ 
+    # İlk sayının karesini çıkarma
+    n2 = n1 * n1;
+ 
+    # İkinci sayıyı elde edecek fonksiyon
+    n1 = (n1 * 2) + 1;
+ 
+    # Aralıktaki tam kare sayıları işleyen döngünün tanımlanması
+    while ((n2 >= ilk and n2 <= son)) :
+ 
+        # Tam kareyi yazdırma
+        print(n2, end= " ");
+ 
+        # Bir sonraki numarayı elde etme
+        n2 = n2 + n1;
+ 
+        # Eklenecek tek sayıyı ilave etme
+        n1 += 2;
+ 
+# Sürücü kod
+if __name__ == "__main__" :
+ 
+    ilk = int(input("Ilk aralik sayisini giriniz: ")); 
+    son = int(input("Son aralik sayisini giriniz: "));
+ 
+    tamKare(ilk, son);
